15132c123bd7a3824a5c3713d15effc87cf45bcf
Brian Ishii <brianlishii@gmail.com>
2018-12-18 13:59:03 -0800

    added table View cell

---
+++ BudgetMe/PurchaseTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PurchaseTableViewCell.swift
+//  BudgetMe
+//
+//  Created by Brian Ishii on 12/18/18.
+//  Copyright © 2018 Brian Ishii. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseTableViewCell: UITableViewCell {
+
+    
+}
